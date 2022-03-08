640d354d1c4626beeae5e747130ba6fb991b7a23
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-08 16:43:57 -0600

    Added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My plans</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />}/>
+          <Route path='/about' element={<Home />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
