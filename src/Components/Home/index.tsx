4f2075c756db88f19310f9465c298ec45869a2b4
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-10 12:28:30 -0600

    Added filter to homepage

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { useState, ChangeEvent } from 'react'
 import PlansList from '../PlansList';
 
 const Home = () => {
+  const [planID, setPlanID] = useState('');
+
+  const handlePlanIDOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+
+    setPlanID(value);
+  }
+
   return (
     <div>
       <h1>Home</h1>
-      <PlansList />
+      <div>
+        <label>
+          Plan ID
+          <input value={planID} onChange={handlePlanIDOnChange} />
+        </label>
+      </div>
+      <PlansList planFilter={planID.toLocaleLowerCase()} />
     </div>
   )
 }
--- src/Components/PlansList/index.tsx
+++ src/Components/PlansList/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import { FC, useEffect, useState } from 'react';
 import './styles.scss';
 import Button from '../Button';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { StoreStateType } from '../../stores/rootReducers';
-import { parsePlanID } from '../../stores/actions/PlansActions';
+import { parsePlanID, Plan } from '../../stores/actions/PlansActions';
+import { PlansListProps } from './interface';
 
-export default function PlansList() {
+const PlansList: FC<PlansListProps> = ({ planFilter }) => {
   const navigate = useNavigate();
   const { plans } = useSelector<StoreStateType, StoreStateType>((state) => state);
+  const [filteredMaps, setFilteredMaps] = useState<Plan[]>([]);
+
+  useEffect(() => {
+    setFilteredMaps(plans.filter(plan => parsePlanID(plan.id).toLocaleLowerCase().includes(planFilter)));
+  }, [planFilter, plans]);
 
   return (
     <div>
       <div className='btn-list'>
-        {plans.map(plan => (
+        {filteredMaps.length > 0
+        ? filteredMaps.map(plan => (
           <Button key={plan.id} className='' onClick={() => navigate(`/plan/${plan.id}`)}>
             {parsePlanID(plan.id)}
           </Button>
-        ))}
+        ))
+        : 'No plans found'
+        }
       </div>
     </div>
   )
 }
+
+
+export default PlansList;
